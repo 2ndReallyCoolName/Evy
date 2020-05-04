1120c75550e35248b007aa7ee61fd383a81d6494
sking71823 <58529786+sking71823@users.noreply.github.com>
2020-05-04 12:52:52 -0400

    Add files via upload

---
+++ Tests/NormalizeVectorTest.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Vector vector1 = new Vector(25, 32);
+
+            vector1.Normalize();
+
+            return vector1;
+            
+            
+
+            
+           
+
+
+
+
+
+        }
+
+
+    }
+}
+
