31acb5851f50943121cfbc86b9387813aaf29f13
sking71823 <58529786+sking71823@users.noreply.github.com>
2020-05-04 01:42:04 -0400

    Add files via upload

---
+++ Tests/Nearestpowerof2test.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int pwr = 1;
+            int x;
+
+            while (pwr <= x)
+            {
+                pwr = pwr << 1;
+            }
+
+            return pwr;
+
+
+
+        }
+
+
+    }
+}
+
