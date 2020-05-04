e42b9c3df7e8140b07a341cc730409d1b253f6b3
sking71823 <58529786+sking71823@users.noreply.github.com>
2020-05-04 04:05:57 -0400

    Add files via upload

---
+++ Tests/NegatevectorTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Vector vec1 = new Vector(27, 50);
+            
+            Vector negate = new Vector();
+
+            negate = vec1 * -1;
+
+            return vec1;
+
+
+
+
+
+        }
+
+
+    }
+}
+
